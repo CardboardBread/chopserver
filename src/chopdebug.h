62fee4e125c52f93965da3048d49015317462a91
Michael Brown <michaelmbcubed@gmail.com>
2020-03-10 21:59:22 -0400

    added new macro for printing properly to stdout can be modified to require release definition in cmake

--- src/chopdebug.h
+++ src/chopdebug.h
@@ -9,33 +9,34 @@
  */
 
 static const int debug_fd = STDERR_FILENO;
+static const int msg_fd = STDOUT_FILENO;
 static const char dbg_head[] = "[DEBUG]: ";
 static const char dbg_fcn_head[] = "[DEBUG][%s]: ";
-static const char dbg_fcn_tail[] = "\n";
 static const char dbg_err[] = "[ERRNO %d]: %s\n";
 static const char dbg_pack[] = "PACKET: {%d:%s:%d:%d}";
+static const char msg_tail[] = "\n";
 
 extern int header_type;
-static const char msg_tail[] = "\n";
 
 static const char recv_text_start[] = ": \"";
 static const char recv_text_seg[] = "%.*s";
 static const char recv_text_end[] = "\"\n";
 
-static const char recv_ping_norm[] = " ENQUIRY\n";
-static const char recv_ping_send[] = " ENQURIY Requested\n";
-static const char recv_ping_time[] = " Time ENQUIRY: \"%ld\"\n";
-static const char recv_ping_time_send[] = " Time ENQUIRY Requested\n";
+static const char recv_ping_norm[] = "ENQUIRY\n";
+static const char recv_ping_send[] = "ENQURIY Requested\n";
+static const char recv_ping_time[] = "Time ENQUIRY: \"%ld\"\n";
+static const char recv_ping_time_send[] = "Time ENQUIRY Requested\n";
 
-static const char ackn_text[] = " %s Confirmed\n";
+static const char ackn_text[] = "%s Confirmed\n";
 
-static const char wakeup_text[] = " Requesting Wakeup\n";
+static const char wakeup_text[] = "Requesting Wakeup\n";
 
-static const char neg_ackn_text[] = " %s Refused\n";
+static const char neg_ackn_text[] = "%s Refused\n";
 
-static const char idle_text[] = " Requesting Idle\n";
+static const char idle_text[] = "Requesting Idle\n";
 
-static const char esc_text[] = " Requesting Disconnect\n";
+static const char esc_text[] = "Requesting Disconnect\n";
+static const char esc_confirm[] = "Connection closed.\n";
 
 /*
  * Prints requested format string into stderr, prefixing properly
@@ -69,7 +70,15 @@ const char *msg_header();
 #ifdef DEBUG
 #define DEBUG_PRINT(fmt, args...) _debug_print(__FUNCTION__, fmt, ## args)
 #else
-#define DEBUG_PRINT(fmt, args...) {} /* Don't do anything in release builds */
+#define DEBUG_PRINT(fmt, args...) {} /* Don't do anything in non-debug builds */
+#endif
+
+#define RELEASE 7
+
+#ifdef RELEASE
+#define MESSAGE_PRINT(fd, fmt, args...) _message_print(fd, fmt, ## args)
+#else
+#define MESSAGE_PRINT(fmt, args...) {} /* Don't do anything in non-release builds */
 #endif
 
 #endif
