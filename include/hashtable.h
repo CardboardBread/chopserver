6cf1535d9ed5d8952b436da7349907411425ddc3
Michael Brown <michaelmbcubed@gmail.com>
2020-11-02 23:48:52 -0500

    exposed hashtable functions

--- include/hashtable.h
+++ include/hashtable.h
@@ -1,6 +1,9 @@
 #ifndef CHOPSERVER_HASHTABLE_H
 #define CHOPSERVER_HASHTABLE_H
 
+#include <stddef.h>
+#include <stdbool.h>
+
 /*
  * Data Types
  */
@@ -14,5 +17,15 @@ typedef struct hash_table_str hash_table_t;
 /*
  * Functions
  */
+
+struct hash_table_str *table_create(size_t size);
+
+bool table_destroy(struct hash_table_str *target, size_t *size_dest);
+
+bool table_get(struct hash_table_str *target, hash_key key, hash_value *dest);
+
+bool table_put(struct hash_table_str *target, hash_key key, hash_value value);
+
+bool table_remove(struct hash_table_str *target, hash_key key, hash_value *dest);
 
 #endif //CHOPSERVER_HASHTABLE_H
